3f693e91c29cc95359f8658bc5d577f1bcb89969
Stepan Saprunov <lawrencecrafto@gmail.com>
2022-02-12 13:40:44 +0300

    added mobile support

--- chart.js
+++ chart.js
@@ -1,50 +1,69 @@
 /*
    + нарисовать градиент
-   - нарисовать график
+   - нарисовать оси
    - добавить точки
-   - сделать так чтобы точки можно было двигать вертикально
+   - сделать так чтобы точки можно было двигать 
    - соединить точки линиями
    - добавить возможность увеличивать график
 */
 
+function vhToPixels (vh) {
+    return Math.round(window.innerHeight / (100 / vh));
+}
+function vwToPixels (vw) {
+    return Math.round(window.innerWidth / (100 / vw));
+}
+
 class Chart {
 
-    constructor (canvas, width, height, dots) {
+    constructor (canvas, dots, mobile) {
         this.canvas = canvas;
         this.context = canvas.getContext("2d");
-        this.canvas.width = width;
-        this.canvas.height = height;
+        this.mobile = mobile;
+        if (mobile) {
+            this.canvas.width = vwToPixels(80);
+            this.canvas.height = vhToPixels(80);
+            this.width = this.canvas.width;
+            this.height = this.canvas.height;
+            this.gradient = this.context.createLinearGradient(this.width, 0, 0, 0);
+        }
+        else {
+            this.canvas.width = vwToPixels(80);
+            this.canvas.height = vwToPixels(80)/2;
+            this.width = this.canvas.width;
+            this.height = this.canvas.height;
+            this.gradient = this.context.createLinearGradient(0,0, 0, this.height);
+        }
         this.dotsNumber = dots;
-        this.width = width;
-        this.height = height;
         this.#drawGradient();
     }
 
     #drawGradient() {
-        this.gradient = this.context.createLinearGradient(0,0, 0, this.height);
         this.gradient.addColorStop(0, "red");
         this.gradient.addColorStop(1, "blue");
         this.context.fillStyle = this.gradient;
         this.context.fillRect(10, 10, this.width-20, this.height-20);
-        this.context.strokeRect(10, 10, this.width-20, this.height-20);
         this.context.strokeRect(0, 0, this.width, this.height);
+    }
+
+    #drawaxis() {
+        if (this.mobile) {
+
+        }
     }
 }
 
-let chart = new Chart(document.getElementById("chart"), 400, 200, 4);
+
+
+let chart = new Chart(
+    document.getElementById("chart"), 
+    4, 
+    vwToPixels(1) < vhToPixels(1));
 let scroll0 = document.getElementById("scroll0");
 scroll0.addEventListener("click", ()=>{
-    document.getElementById("emotions-block").scrollIntoView();
+    document.getElementById("chart-block").scrollIntoView();
 })
 let scroll1 = document.getElementById("scroll1");
 scroll1.addEventListener("click", ()=>{
     document.getElementById("who-block").scrollIntoView();
-})
-let scroll2 = document.getElementById("scroll2");
-scroll2.addEventListener("click", ()=>{
-    document.getElementById("chart-block").scrollIntoView();
-})
-let scroll3 = document.getElementById("scroll3");
-scroll3.addEventListener("click", ()=>{
-    document.getElementById("emotions-block").scrollIntoView();
 })
