bbdcfc788b2c25666d7fb887aa8bbb43a5a33efa
Stepan Saprunov <lawrencecrafto@gmail.com>
2022-02-14 02:48:00 +0300

    added strings, chart finished

--- chart.js
+++ chart.js
@@ -1,11 +1,3 @@
-/*
-   + нарисовать градиент
-   + нарисовать оси
-   + добавить точки
-   - сделать так чтобы точки можно было двигать 
-   - добавить возможность увеличивать график
-*/
-
 function vhToPixels (vh) {
     return Math.round(window.innerHeight / (100 / vh));
 }
@@ -30,7 +22,7 @@ class Chart {
         this.context = canvas.getContext("2d");
         this.mobile = mobile;
         if (mobile) {
-            this.canvas.width = vwToPixels(100);
+            this.canvas.width = vwToPixels(90);
             this.canvas.height = vhToPixels(80);
             this.width = this.canvas.width;
             this.height = this.canvas.height;
@@ -38,23 +30,23 @@ class Chart {
         }
         else {
             this.canvas.width = vwToPixels(60);
-            this.canvas.height = vhToPixels(80);
+            this.canvas.height = vhToPixels(40);
             this.width = this.canvas.width;
             this.height = this.canvas.height;
             this.gradient = this.context.createLinearGradient(0,0, 0, this.height);
         }
         if (this.mobile) {
-            let step = (this.height - 40*2 - 10 - 10)/(this.dots.length);
+            let step = (this.height - 60)/(this.dots.length+1);
             this.dots.forEach((el, i)=>{
                 el.x = this.width/2 + el.offset;
-                el.y = 10+i*step + 40*2 + 10;
+                el.y = i*step + 60;
                 el.radius = 10;
             })
         }
         else {
-            let step = (this.width - 40*2 - 10 - 10)/(this.dots.length + 1);
+            let step = (this.width - 60)/(this.dots.length + 1);
             for (let i = 0; i < this.dots.length; i++) {
-                this.dots[i].x = 10+i*step + 40*2 + 10;
+                this.dots[i].x = i*step + 60;
                 this.dots[i].y = this.height/2 + this.dots[i].offset;
                 this.dots[i].radius = 10;
             }
@@ -141,61 +133,141 @@ class Chart {
         this.gradient.addColorStop(0, "red");
         this.gradient.addColorStop(1, "blue");
         this.context.fillStyle = this.gradient;
-        this.context.fillRect(40, 40, this.width-80, this.height-80);
+        this.context.fillRect(0, 0, this.width, this.height);
     }
 
     #drawAxis() {
         if (this.mobile) {
             this.context.lineWidth = 5;
             this.context.beginPath();
-            this.context.moveTo(this.width/2, 40);
-            this.context.lineTo(this.width/2, this.height-40);
+            this.context.moveTo(this.width/2, 0);
+            this.context.lineTo(this.width/2, this.height);
             this.context.stroke();
             this.context.beginPath();
-            this.context.moveTo(40, 50);
-            this.context.lineTo(this.width-40, 50);
+            this.context.moveTo(0, 20);
+            this.context.lineTo(this.width, 20);
             this.context.stroke();
         }
         else {
             this.context.lineWidth = 5;
             this.context.beginPath();
-            this.context.moveTo(40, this.height/2);
-            this.context.lineTo(this.width-40, this.height/2);
+            this.context.moveTo(0, this.height/2);
+            this.context.lineTo(this.width, this.height/2);
             this.context.stroke();
             this.context.beginPath();
-            this.context.moveTo(50, 40);
-            this.context.lineTo(50, this.height-40);
+            this.context.moveTo(20, 0);
+            this.context.lineTo(20, this.height);
             this.context.stroke();
         }
     }
 
     #drawDots() {
         if (this.mobile) {
-            let step = (this.height - 40*2 - 10 - 10)/(this.dots.length);
+            let step = (this.height - 10 - 10)/(this.dots.length);
             this.dots.forEach((el, i)=>{
+                if (i < this.dots.length-1) {
+                    this.context.beginPath();
+                    this.context.moveTo(el.x, el.y);
+                    this.context.lineTo(this.dots[i+1].x, this.dots[i+1].y);
+                    this.context.stroke();
+                }
                 this.context.beginPath();
                 this.context.arc(el.x, el.y, el.radius, 0, Math.PI*2, false);
                 this.context.stroke();
             })
         }
         else {
-            let step = (this.width - 40*2 - 10 - 10)/(this.dots.length + 1);
-            for (let i = 0; i < this.dots.length; i++) {
+            let step = (this.width - 10 - 10)/(this.dots.length + 1);
+            this.dots.forEach((el, i)=>{
+                if (i < this.dots.length-1) {
+                    this.context.beginPath();
+                    this.context.moveTo(el.x, el.y);
+                    this.context.lineTo(this.dots[i+1].x, this.dots[i+1].y);
+                    this.context.stroke();
+                }
                 this.context.beginPath();
-                this.context.arc(this.dots[i].x, this.dots[i].y, this.dots[i].radius, 0, Math.PI*2, false);
+                this.context.arc(el.x, el.y, el.radius, 0, Math.PI*2, false);
                 this.context.stroke();
+            })
+        }
+    }
+
+    addPoint() {
+        if (this.dots.length < 20) {
+            if (this.mobile) {
+                this.dots.push({
+                    offset: 0,
+                    x: this.width/2,
+                    y: 0,
+                    radius: 10,
+                });
+            }
+            else {
+                this.dots.push({
+                    offset: 0,
+                    x: 0,
+                    y: this.height/2,
+                    radius: 10,
+                });
+            }
+            this.#updateDotsCoordinates();
+            this.#clearCanvas();
+            this.#doDrawing();
+            return true;
+        }
+        return false;
+    }
+
+    removePoint() {
+        if (this.dots.length > 1)  {
+            this.dots.pop();
+            this.#updateDotsCoordinates();
+            this.#clearCanvas();
+            this.#doDrawing();
+            return true;
+        } 
+        return false;
+    }
+
+    #updateDotsCoordinates() {
+        if (this.mobile) {
+            let step = (this.height-60)/(this.dots.length);
+            this.dots.forEach((el, i)=>{
+                el.y = 60+i*step;
+                el.radius = 10;
+            })
+        }
+        else {
+            let step = (this.width-60)/(this.dots.length);
+            for (let i = 0; i < this.dots.length; i++) {
+                this.dots[i].x = 60+i*step;
+                this.dots[i].radius = 10;
             }
         }
     }
 }
 
-if (vwToPixels(1) < vhToPixels(1)) {
-    document.getElementsByTagName("body")[0].classList.add("mobile");
-    document.getElementsByClassName("chart")[0].classList.add("mobile-chart");
-}
-
 let chart = new Chart(
     document.getElementById("chart"), 
-    10, 
+    4, 
     vwToPixels(1) < vhToPixels(1));
 
+document.getElementById("dot-increase").addEventListener("click", ()=>{
+    if (chart.addPoint()) {
+        let element = document.createElement('input');
+        let parent = document.getElementById("strings-container");
+        element.setAttribute("type", "text");
+        element.setAttribute("name", 
+            "string"+(parent.childElementCount));
+        element.setAttribute("id",
+            "string"+(parent.childElementCount));
+        element.classList.add("string");
+        parent.insertAdjacentElement("beforeend", element);
+    }
+})
+document.getElementById("dot-decrease").addEventListener("click", ()=>{
+    if (chart.removePoint()) {
+        let parent = document.getElementById("strings-container");
+        parent.removeChild(parent.lastElementChild);
+    }
+})
